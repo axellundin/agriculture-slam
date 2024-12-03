73d7a3c5c9974058d2cfad2518f34400f24a1507
axellundin <axellundin@gmail.com>
2024-12-03 14:38:55 +0100

    Created docker container and script for downloading data.

---
+++ slam-argiculture/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Download the dataset if it doesn't exist
+if [ ! -f bonirob_2016-04-20-15-43-50_10.bag ]; then
+    echo "Downloading dataset..."
+    mkdir -p data/bags
+    curl -o data/bags/bonirob_2016-04-20-15-43-50_10.bag https://www.ipb.uni-bonn.de/datasets_IJRR2017/rosbags/160420/bonirob_2016-04-20-15-43-50_10.bag
+fi
+
+# Keep the container running
+tail -f /dev/null
