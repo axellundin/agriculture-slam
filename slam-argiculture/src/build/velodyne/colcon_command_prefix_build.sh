1a1a62f7e336d603ddb7aea6e7b702c0512028b3
axellundin <axellundin@gmail.com>
2024-12-16 10:57:39 +0100

    Removed old files, started new project

--- slam-argiculture/src/build/velodyne/colcon_command_prefix_build.sh
+++
@@ -1,5 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/ros/install/velodyne_laserscan/share/velodyne_laserscan/package.sh"
-. "/ros/install/velodyne_msgs/share/velodyne_msgs/package.sh"
-. "/ros/install/velodyne_driver/share/velodyne_driver/package.sh"
-. "/ros/install/velodyne_pointcloud/share/velodyne_pointcloud/package.sh"
--- slam-argiculture/src/build/velodyne_pointcloud/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/ros/install/velodyne_msgs/share/velodyne_msgs/package.sh"
